e8610ccac6d80134234491d77498a3458f949917
sw7190 <sw7190@naver.com>
2018-06-14 13:29:53 +0900

    1ㄷ1채팅,대화목록 기능 추가

---
+++ db/ChatModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+let chatSchema = mongoose.Schema({ //friends collection Schema
+    id : String,
+    name : String,
+    fid : String,
+    fname : String,
+    msg : String
+});
+
+module.exports = mongoose.model('chat', chatSchema, 'chat');
